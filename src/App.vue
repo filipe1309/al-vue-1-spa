4abb12b44467085845d18e79b1d3d5f4d0244a07
Filipe Leuch Bonfim <filipe1309@gmail.com>
2021-01-16 01:22:16 -0300

    feat: add menu

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,28 @@
 <template>
   <div class="corpo">
     
-    <router-view></router-view>
+    <nav>
+      <ul>
+        <li v-for="route in routes"><router-link :to="route.path ? route.path : '/'">{{ route.titulo }}</router-link></li>
+      </ul>
+    </nav>
+
+    <transition name="pagina">
+      <router-view></router-view>
+    </transition>
 
   </div>
 </template>
 
 <script>
+import { routes } from './routes';
 
 export default {
-  
+  data() {
+    return {
+      routes
+    }
+  }
 }
 
 </script>
@@ -20,6 +33,14 @@ export default {
     font-family: Helvetica, sans-serif;
     width: 96%;
     margin: 0 auto;
+  }
+
+  .pagina-enter, .pagina-leave-active {
+    opacity: 0;
+  }
+
+  .pagina-enter-active, .pagina-leave-active {
+    transition: opacity .2s;
   }
 
 </style>
