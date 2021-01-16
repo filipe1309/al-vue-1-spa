eb04c5be0b2b0c397979becee63b75e1a271b701
Filipe Leuch Bonfim <filipe1309@gmail.com>
2021-01-16 14:44:21 -0300

    feat: add Button component

---
+++ src/components/shared/botao/Botao.vue
@@ -0,0 +1,30 @@
+<template>
+  <button class="botao botao-perigo" :type="tipo">{{ rotulo }}</button>
+</template>
+
+<script>
+export default {
+  props: ['tipo ', 'rotulo']
+}
+</script>
+
+<style>
+  .botao {
+    display: inline-block;
+    padding: 10px;
+    border-radius: 3px;
+    margin: 10px;
+    font-size: 1.2em;
+  }
+
+  .botao-perigo {
+    background-color: firebrick;
+    color: white;
+  }
+
+  .botao-padrao {
+    background-color: darkcyan;
+    color: white;
+  }
+
+</style>
